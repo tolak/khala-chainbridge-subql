a97078df1d0d94b1c56d1f06afd587401972edb8
Wenfeng Wang <kalot.wang@gmail.com>
2021-10-25 14:57:54 +0800

    index revenue from treasury

---
+++ src/mappings/revenue.ts
@@ -0,0 +1,23 @@
+import { Revenue } from '../types/models/Revenue'
+import { SubstrateEvent } from "@subql/types"
+import { IEvent } from '@polkadot/types/types'
+import { U128 } from '@polkadot/types'
+
+export async function handleTreasuryDeposit(event: SubstrateEvent): Promise<void> {
+    let blockHeight = event.block.block.header.number.toBigInt()
+    let revenue = await Revenue.get(`treasury-${blockHeight.toString()}`)
+    if (revenue === undefined) {
+        revenue = new Revenue(`treasury-${blockHeight.toString()}`)
+        revenue.blockHeight = blockHeight
+    }
+    if (revenue.blockHeight !== blockHeight) throw new Error('Block number dismatch, qed.')
+
+    if (event.event.section === 'treasury' && event.event.method === 'Deposit') {
+        const {
+            data: [encodedAmount],
+        } = event.event as unknown as IEvent<[U128]>
+        revenue.amount += encodedAmount.toBigInt()
+    }
+
+    await revenue.save()
+}
