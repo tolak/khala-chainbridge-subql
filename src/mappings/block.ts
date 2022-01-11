269eba8165f98002ada974f37700529cbf22aa27
Wenfeng Wang <kalot.wang@gmail.com>
2022-01-11 22:12:22 +0800

    new bridge indexing & some refactors

---
+++ src/mappings/block.ts
@@ -0,0 +1,43 @@
+import { SubstrateBlock } from '@subql/types'
+import { SpecVersion, Circulation } from '../types'
+import { AccountInfo, Balance } from "@polkadot/types/interfaces";
+
+let lastUpdatedBlock = BigInt(0);
+export async function handleBlock(block: SubstrateBlock): Promise<void> {
+    let blockHeight = block.block.header.number.toBigInt()
+
+    // Update spec version
+    const specVersion = await SpecVersion.get(block.specVersion.toString())
+    if (specVersion === undefined) {
+        const newSpecVersion = new SpecVersion(block.specVersion.toString())
+        newSpecVersion.blockHeight = blockHeight
+        await newSpecVersion.save()
+    }
+
+    // Circulation computing, updated every 100 blocks
+    let circulation = await Circulation.get(`circulation-${lastUpdatedBlock.toString()}`)
+    if (circulation === undefined || (blockHeight - lastUpdatedBlock) === BigInt(100)) {
+        try {
+            // query onchain storage
+            await api.queryMulti([
+                api.query.balances.totalIssuance,
+                [api.query.system.account, '5EYCAe5iixJKLJE5vokZcdJwS4ZpFU23Ged95YDBznC789dM'],
+                [api.query.system.account, '5EYCAe5iixJKLJE7D1zaaRxUiy2bL4KUKqZBSckPw3iWSyvk']
+            ], async ([b_totalSupply, b_bridgeReserved, b_miningSubsidy]) => {
+                let totalSupply = (b_totalSupply as Balance).toBigInt()
+                let bridgeReserved = ((b_bridgeReserved as AccountInfo).data.free as Balance).toBigInt()
+                let miningSubsidy = ((b_miningSubsidy as AccountInfo).data.free as Balance).toBigInt()
+                circulation = new Circulation(`circulation-${blockHeight.toString()}`)
+                circulation.khala = totalSupply - bridgeReserved - miningSubsidy
+                circulation.total = totalSupply - miningSubsidy
+                circulation.blockHeight = blockHeight
+                await circulation.save()
+                logger.debug(`Save circulation [khala: ${circulation.khala.toString()}, total: ${circulation.total.toString()}] at block ${blockHeight.toString()}`)
+                lastUpdatedBlock = blockHeight;
+            });
+        } catch(e) {
+            logger.error(`Circulation error: ${e}`)
+            
+        }
+    }
+}
---
+++ src/mappings/xtransfer.ts
@@ -0,0 +1,148 @@
+import { Bytes, U256 } from '@polkadot/types'
+import { IEvent } from '@polkadot/types/types'
+import { SubstrateEvent } from '@subql/types'
+import { AccountId } from "@polkadot/types/interfaces";
+import { BridgeChainId, DepositNonce, ResourceId } from '../interfaces'
+import { BridgeOutboundingRecord, BridgeInboundingRecord, Tx } from '../types'
+
+export async function handleFungibleTransferEvent(ctx: SubstrateEvent): Promise<void> {
+    const {
+        data: [chainIdCodec, depositNonceCodec, resourceId, amount, recipient],
+    } = ctx.event as unknown as IEvent<[BridgeChainId, DepositNonce, ResourceId, U256, Bytes]>
+
+    const chainId = chainIdCodec.toNumber()
+    const depositNonce = depositNonceCodec.toBigInt()
+
+    const id = `${chainId}-${depositNonce}`
+
+    if (undefined === (await BridgeOutboundingRecord.get(id))) {
+        const record = new BridgeOutboundingRecord(id)
+        record.createdAt = ctx.block.timestamp
+        record.destChainId = chainId
+        record.depositNonce = depositNonce
+        record.resourceId = resourceId.toHex()
+        record.amount = amount.toBigInt()
+        record.recipient = recipient.toHex()
+        record.sender = ctx.extrinsic?.extrinsic.signer.toString()
+
+        let txId = ctx.extrinsic?.extrinsic.hash.toHex()
+        let sendTx = new Tx(txId)
+        sendTx.hash = ctx.extrinsic?.extrinsic.hash.toHex()
+        sendTx.sender = ctx.extrinsic?.extrinsic.signer.toString()
+        await sendTx.save()
+
+        record.sendTx = txId
+        await record.save()
+        logger.debug(`Created new outbounding record: ${record}`)
+    }
+}
+
+export async function handleProposalVoteForEvent(ctx: SubstrateEvent): Promise<void> {
+    const {
+        data: [chainIdCodec, depositNonceCodec, _voter],
+    } = ctx.event as unknown as IEvent<[BridgeChainId, DepositNonce, AccountId]>
+
+    const originChainId = chainIdCodec.toNumber()
+    const depositNonce = depositNonceCodec.toBigInt()
+
+    const id = `${originChainId}-${depositNonce}`
+    let record = await BridgeInboundingRecord.get(id)
+    if (record === undefined) {
+        record = new BridgeInboundingRecord(id)
+        record.createdAt = ctx.block.timestamp
+        record.originChainId = originChainId
+        record.depositNonce = depositNonce
+        record.resourceId = ctx.extrinsic?.extrinsic.args[2].toHex()
+        record.status = 'Initiated'
+        record.voteTxs = []
+        logger.debug(`Created new inbounding record: ${record}`)
+    }
+
+    let txId = ctx.extrinsic.extrinsic.hash.toHex()
+    let voteTx = new Tx(txId)
+    voteTx.hash = ctx.extrinsic.extrinsic.hash.toHex()
+    voteTx.sender = ctx.extrinsic?.extrinsic.signer.toString()
+    await voteTx.save()
+
+    let votes = record.voteTxs
+    votes.push(txId)
+    record.voteTxs = votes
+    await record.save()
+    logger.debug(`Add new vote into inbounding record: ${record}`)
+}
+
+export async function handleProposalApprovedEvent(ctx: SubstrateEvent): Promise<void> {
+    const {
+        data: [chainIdCodec, depositNonceCodec],
+    } = ctx.event as unknown as IEvent<[BridgeChainId, DepositNonce]>
+
+    const originChainId = chainIdCodec.toNumber()
+    const depositNonce = depositNonceCodec.toBigInt()
+
+    const id = `${originChainId}-${depositNonce}`
+    let record = await BridgeInboundingRecord.get(id)
+    if (record !== undefined) {
+        record.status = 'Approved'
+        await record.save()
+        logger.debug(`Inbounding record approved: ${id}`)
+    }
+}
+
+export async function handleProposalSucceededEvent(ctx: SubstrateEvent): Promise<void> {
+    const {
+        data: [chainIdCodec, depositNonceCodec],
+    } = ctx.event as unknown as IEvent<[BridgeChainId, DepositNonce]>
+
+    const originChainId = chainIdCodec.toNumber()
+    const depositNonce = depositNonceCodec.toBigInt()
+
+    const id = `${originChainId}-${depositNonce}`
+    let record = await BridgeInboundingRecord.get(id)
+    if (record !== undefined) {
+        record.status = 'Succeeded'
+
+        let txId = ctx.extrinsic?.extrinsic.hash.toHex()
+        let executeTx = new Tx(txId)
+        executeTx.hash = ctx.extrinsic?.extrinsic.hash.toHex()
+        executeTx.sender = ctx.extrinsic?.extrinsic.signer.toString()
+        await executeTx.save()
+
+        record.executeTx = txId
+        await record.save()
+        logger.debug(`Inbounding record succeeded: ${id}, with execute tx: ${executeTx}`)
+    }
+}
+
+export async function handleProposalRejectedEvent(ctx: SubstrateEvent): Promise<void> {
+    const {
+        data: [chainIdCodec, depositNonceCodec],
+    } = ctx.event as unknown as IEvent<[BridgeChainId, DepositNonce]>
+
+    const originChainId = chainIdCodec.toNumber()
+    const depositNonce = depositNonceCodec.toBigInt()
+
+    const id = `${originChainId}-${depositNonce}`
+    let record = await BridgeInboundingRecord.get(id)
+    if (record !== undefined) {
+        record.status = 'Rejected'
+        await record.save()
+        logger.debug(`Inbounding record rejected: ${id}`)
+    }
+}
+
+export async function handleProposalFailedEvent(ctx: SubstrateEvent): Promise<void> {
+    const {
+        data: [chainIdCodec, depositNonceCodec],
+    } = ctx.event as unknown as IEvent<[BridgeChainId, DepositNonce]>
+
+    const originChainId = chainIdCodec.toNumber()
+    const depositNonce = depositNonceCodec.toBigInt()
+
+    const id = `${originChainId}-${depositNonce}`
+    let record = await BridgeInboundingRecord.get(id)
+    if (record !== undefined) {
+        record.status = 'Failed'
+        await record.save()
+        logger.debug(`Inbounding record failed: ${id}`)
+    }
+}
